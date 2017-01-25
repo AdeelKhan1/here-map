1a861786396fdae1f694506fa190dc0e5a0d70e6
Gonçalo Vieira <sandwave@gmail.com>
2017-01-25 00:01:11 +0000

    Services.
    They store some objects we only want to keep as singletons throughout the app. As well as serve as a store.

---
+++ src/app/shared/platform.service.js
@@ -0,0 +1,8 @@
+export default class PlatformService {
+    constructor() {
+        this.platform = new H.service.Platform({
+            'app_id': 'rjavUTHi8Ro5SdWrQDHf',
+            'app_code': 'b9naScHzM-oPSs6SopEtIw'
+        });
+    }
+}
---
+++ src/app/shared/position.service.js
@@ -0,0 +1,15 @@
+export default class PositionService {
+    constructor($rootScope) {
+        this.$rootScope = $rootScope;
+        this.state = {
+            selectedPosition: {}
+        };
+    }
+    selectPosition(value) {
+        this.state.selectedPosition = value;
+        this.$rootScope.$broadcast('selected.position.updated', this.state.selectedPosition);
+    }
+    getPosition() {
+        return this.state.selectedPosition;
+    }
+}
